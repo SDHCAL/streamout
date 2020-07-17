6ddffc68750800c4d057c8ca9abed34b8071fdb5
GRENIER Gerald <g.grenier@ipnl.in2p3.fr>
2020-07-17 15:43:22 +0200

    Add ascii dump of data

---
+++ interface/standard/include/textDump.h
@@ -0,0 +1,20 @@
+#ifndef textDump_h
+#define textDump_h
+
+#include "SDHCAL_buffer.h"
+
+class textDump
+{
+ public:
+  textDump(std::ostream &out=std::cout) : _out(out) {;}
+  void start();
+  void processDIF(DIFPtr*);
+  void processFrame(DIFPtr*,uint32_t frameIndex);
+  void processPadInFrame(DIFPtr*,uint32_t frameIndex, uint32_t channelIndex);
+  void processSlowControl(SDHCAL_buffer);
+  void end();
+ private:
+  std::ostream & _out;
+};
+
+#endif
---
+++ interface/standard/src/textDump.cc
@@ -0,0 +1,42 @@
+#include "textDump.h"
+
+#include <iostream>
+
+void textDump::start()
+{
+  _out << "Will dump bunch of DIF data" << std::endl;
+}
+
+void textDump::processDIF(DIFPtr* d)
+{
+  if (NULL == d) return;
+  _out << "DIF number is " << d->getDIFid() << std::endl;
+  _out << "  DTC value is " << d->getDTC()  << std::endl;
+  _out << "  GTC value is " << d->getGTC()  << std::endl;
+  _out << "  DIF BCID is " << d->getBCID()  << std::endl;
+  _out << "  Absolute BCID is " << d->getAbsoluteBCID() << std::endl;
+}
+
+void textDump::processFrame(DIFPtr* d,uint32_t frameIndex)
+{
+  _out << "  Displaying frame number " << frameIndex << std::endl;
+  _out << "    ASIC ID is " << d->getASICid(frameIndex) << std::endl;
+  _out << "    Frame BCID is " << d->getFrameBCID(frameIndex)  << std::endl;
+  _out << "    Frame Time To Trigger (a.k.a timestamp) is " << d->getFrameTimeToTrigger(frameIndex) << std::endl;
+}
+
+void textDump::processPadInFrame(DIFPtr* d,uint32_t frameIndex, uint32_t channelIndex)
+{
+  _out << "    Displaying channel number " << channelIndex << std::endl;
+  _out << "      Threshold status is " << d->getThresholdStatus(frameIndex,channelIndex) << std::endl;
+}
+
+void textDump::processSlowControl(SDHCAL_buffer)
+{
+  _out << "textDump::processSlowControl not implemented yet." << std::endl;
+}
+
+void textDump::end()
+{
+  _out << "textDump end of report" << std::endl;
+}
