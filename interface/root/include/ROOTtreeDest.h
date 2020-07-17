9c3d7b08cd7ef3937a6c3b0e72f017adae5a727c
flagarde <flagarde.programs@pm.me>
2020-07-17 18:25:42 +0200

    Add a crude raw data buffer to ROOT TTree converter

--- interface/root/include/ROOTtreeDest.h
+++ interface/root/include/ROOTtreeDest.h
@@ -1,6 +1,31 @@
 #ifndef ROOTtreeDest_h
 #define ROOTtreeDest_h
 
-void myFuncDoingNothing();
+#include "SDHCAL_buffer.h"
+#include "TTree.h"
+
+class ROOTtreeDest
+{
+ public:
+  typedef struct {
+    UInt_t DIFid,ASICid,CHANNELid;
+    UInt_t Thresh;
+    UInt_t DTC,GTC,DIF_BCID,frame_BCID,timeStamp;
+    ULong64_t AbsoluteBCID;
+  } DATA;
+  
+  ROOTtreeDest();
+    
+  void start();
+  void processDIF(DIFPtr*);
+  void processFrame(DIFPtr*,uint32_t frameIndex);
+  void processPadInFrame(DIFPtr*,uint32_t frameIndex, uint32_t channelIndex);
+  void processSlowControl(SDHCAL_buffer) {;}
+  void end() {;}
+ private:
+  DATA _data;
+  TTree *_tree;
+  void dataReset();
+};
 
 #endif
