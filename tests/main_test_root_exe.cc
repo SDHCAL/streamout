6ae6693137b6baefd395f139f6629c2e0ecf9502
flagarde <lagarde@sjtu.edu.cn>
2020-07-20 05:55:21 +0800

    Test the loop and add a event number max to process

--- tests/main_test_root_exe.cc
+++ tests/main_test_root_exe.cc
@@ -9,7 +9,8 @@ int main()
   TFile hfile("Test.root","RECREATE","Test for SDHCAL raw data file");
   DIFdataExample source;
   ROOTtreeDest destination;
-  SDHCAL_buffer_loop(source,destination);
+  SDHCAL_buffer_loop<DIFdataExample,ROOTtreeDest> toto(source,destination,true,std::cout,true);
+  toto.loop(2);
   hfile.Write();
   hfile.Close();
   return 0;
