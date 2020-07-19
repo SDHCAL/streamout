e3313d42eafdf485ddc65928a89b50afe2c0b9d8
flagarde <lagarde@sjtu.edu.cn>
2020-07-19 13:11:44 +0800

    One class per file

--- core/include/SDHCAL_buffer.h
+++ core/include/SDHCAL_buffer.h
@@ -5,10 +5,9 @@
 // Code originally written by Laurent Mirabito
 //
 
-#ifndef SDHCAL_buffer_h
-#define SDHCAL_buffer_h
+#pragma once
 
-#include <utility>
+//#include <utility>
 #include <iostream>
 #include <assert.h>
 #include "DIFSlowControl.h"
@@ -25,120 +24,3 @@ class SDHCAL_buffer : public std::pair<unsigned char*, uint32_t>
   void printBuffer(uint32_t start, uint32_t stop,std::ostream& flux=std::cout); 
   void printBuffer(uint32_t start=0,std::ostream& flux=std::cout) {printBuffer(start,getsize());}
 };
-
-//class to navigate in the raw data buffer
-class SDHCAL_RawBuffer_Navigator
-{
- public:
-  SDHCAL_RawBuffer_Navigator(SDHCAL_buffer b);
-  ~SDHCAL_RawBuffer_Navigator() {if (NULL!=_theDIFPtr) delete _theDIFPtr;}
-  bool validBuffer() {return _DIFstartIndex != 0;}
-  uint32_t getStartOfDIF() {return _DIFstartIndex;}
-  unsigned char* getDIFBufferStart() {return &(_buffer.buffer()[_DIFstartIndex]);}
-  uint32_t getDIFBufferSize() {return _buffer.getsize()-_DIFstartIndex;}
-  SDHCAL_buffer getDIFBuffer() { return SDHCAL_buffer(getDIFBufferStart(),getDIFBufferSize());}
-  DIFPtr* getDIFPtr();
-  uint32_t getEndOfDIFData() { return getDIFPtr()->getGetFramePtrReturn()+3;} 
-  uint32_t getSizeAfterDIFPtr() { return getDIFBufferSize()-getDIFPtr()->getGetFramePtrReturn();}
-  uint32_t getDIF_CRC();
-  bool hasSlowControlData() { return getDIFBufferStart()[getEndOfDIFData()]==0xb1;}
-  SDHCAL_buffer getSCBuffer() { setSCBuffer(); return _SCbuffer;}
-  bool badSCData() {setSCBuffer(); return _badSCdata;}
-  SDHCAL_buffer getEndOfAllData();
-
-
- private:
-  void setSCBuffer();
-  SDHCAL_buffer _buffer,_SCbuffer;
-  uint32_t _DIFstartIndex;
-  DIFPtr* _theDIFPtr;
-  bool _badSCdata;
-};
-
-
-struct SDHCAL_buffer_LoopCounter
-{
-public:
-  int hasSlowControl=0;
-  int hasBadSlowControl=0;
-  std::map<int,int> DIFStarter;
-  std::map<int,int> DIFPtrValueAtReturnedPos;
-  std::map<int,int> SizeAfterDIFPtr;
-  std::map<int,int> SizeAfterAllData;
-  std::map<int,int> NonZeroValusAtEndOfData;
-
-  void printCounter(std::string description, std::map<int,int> & m, std::ostream& out=std::cout);
-  void printAllCounters(std::ostream& out=std::cout);
-};
-
-//function to loop on buffers
-//
-// template class should implement
-// bool SOURCE::next();
-// SDHCAL_buffer SOURCE::getSDHCALBuffer();
-//
-// void DESTINATION::start();
-// void DESTINATION::processDIF(DIFPtr*);
-// void DESTINATION::processFrame(DIFPtr*,uint32_t frameIndex);
-// void DESTINATION::processPadInFrame(DIFPtr*,uint32_t frameIndex, uint32_t channelIndex);
-// void DESTINATION::processSlowControl(SDHCAL_buffer);
-// void DESTINATION::end();
-//
-
-template <typename SOURCE, typename DESTINATION>
-  void SDHCAL_buffer_loop( SOURCE &source, DESTINATION &dest, bool debug=false, std::ostream& out=std::cout, bool verbose=false, std::ostream& verbose_out=std::cout)
-{
-  dest.start();
-  SDHCAL_buffer_LoopCounter c;
-
-  while ( source.next() )
-    {
-      SDHCAL_buffer buffer=source.getSDHCALBuffer();
-      unsigned char* debug_variable_1=buffer.endOfBuffer();
-      SDHCAL_RawBuffer_Navigator bufferNavigator(buffer);
-      unsigned char* debug_variable_2=bufferNavigator.getDIFBuffer().endOfBuffer();
-      if (verbose) verbose_out << "DIF BUFFER END " << (unsigned int *) debug_variable_1 << " " << (unsigned int *) debug_variable_2 << std::endl;
-      if (debug) assert (debug_variable_1 == debug_variable_2);
-      uint32_t idstart=bufferNavigator.getStartOfDIF();
-      if (debug && idstart==0) buffer.printBuffer();
-      c.DIFStarter[idstart]++;
-      if (!  bufferNavigator.validBuffer() ) continue;
-      DIFPtr *d=bufferNavigator.getDIFPtr();
-      if (debug) assert(d!=NULL);
-      if (d!=NULL)
-	{
-	  c.DIFPtrValueAtReturnedPos[bufferNavigator.getDIFBufferStart()[d->getGetFramePtrReturn()] ]++;
-	  if (debug) assert( bufferNavigator.getDIFBufferStart()[d->getGetFramePtrReturn()]==0xa0 );
-	}
-      c.SizeAfterDIFPtr[bufferNavigator.getSizeAfterDIFPtr()]++;
-      dest.processDIF(d);
-      for (uint32_t i=0;i<d->getNumberOfFrames();i++)
-	{
-	  dest.processFrame(d,i);
-	  for (uint32_t j=0;j<64;j++) dest.processPadInFrame(d,i,j);
-	}
-
-      bool processSC=false;
-      if (bufferNavigator.hasSlowControlData()) {c.hasSlowControl++;processSC=true;}
-      if (bufferNavigator.badSCData()) {c.hasBadSlowControl++;processSC=false;}
-      if (processSC) {dest.processSlowControl(bufferNavigator.getSCBuffer());}
-
-      SDHCAL_buffer eod=bufferNavigator.getEndOfAllData();
-      c.SizeAfterAllData[eod.getsize()]++;
-      unsigned char* debug_variable_3=eod.endOfBuffer();
-      if (verbose) verbose_out << "END DATA BUFFER END " << (unsigned int *) debug_variable_1 << " " << (unsigned int *) debug_variable_3 << std::endl;
-      if (debug) assert (debug_variable_1 == debug_variable_3);
-      if (verbose) {verbose_out << "End of Data remaining stuff : "; eod.printBuffer();}
-
-      int nonzeroCount=0;
-      for (unsigned char* it=eod.buffer(); it != eod.endOfBuffer(); it++)
-	if (int(*it) !=0) nonzeroCount++;
-      c.NonZeroValusAtEndOfData[nonzeroCount]++;
-    } //end of while loop
-
-  dest.end();
-  c.printAllCounters(out);
-}
-
-
-#endif
---
+++ core/src/SDHCAL_RawBuffer_Navigator.cc
@@ -0,0 +1,121 @@
+#include "SDHCAL_RawBuffer_Navigator.h"
+
+SDHCAL_RawBuffer_Navigator::SDHCAL_RawBuffer_Navigator(const SDHCAL_buffer& b) : m_Buffer(b),m_SCbuffer(0,0)
+{
+  m_DIFstartIndex=DIFUnpacker::getStartOfDIF(m_Buffer.buffer(),m_Buffer.getsize(),92);
+}
+
+SDHCAL_RawBuffer_Navigator::~SDHCAL_RawBuffer_Navigator()
+{
+  if(m_TheDIFPtr!=nullptr) delete m_TheDIFPtr;
+}
+
+bool SDHCAL_RawBuffer_Navigator::validBuffer()
+{ 
+  return m_DIFstartIndex != 0;
+}
+
+std::uint32_t SDHCAL_RawBuffer_Navigator::getStartOfDIF() 
+{
+  return m_DIFstartIndex;
+}
+
+unsigned char* SDHCAL_RawBuffer_Navigator::getDIFBufferStart()
+{
+  return &(m_Buffer.buffer()[m_DIFstartIndex]);
+}
+
+std::uint32_t SDHCAL_RawBuffer_Navigator::getDIFBufferSize()
+{
+  return m_Buffer.getsize()-m_DIFstartIndex;
+}
+
+SDHCAL_buffer SDHCAL_RawBuffer_Navigator::getDIFBuffer() 
+{ 
+  return SDHCAL_buffer(getDIFBufferStart(),getDIFBufferSize());
+}
+
+DIFPtr* SDHCAL_RawBuffer_Navigator::getDIFPtr()
+{
+  if(m_TheDIFPtr==nullptr) m_TheDIFPtr=new DIFPtr(getDIFBufferStart(),getDIFBufferSize());
+  return m_TheDIFPtr;
+}
+
+std::uint32_t SDHCAL_RawBuffer_Navigator::getEndOfDIFData()
+{ 
+  return getDIFPtr()->getGetFramePtrReturn()+3;
+} 
+
+std::uint32_t SDHCAL_RawBuffer_Navigator::getSizeAfterDIFPtr() 
+{ 
+  return getDIFBufferSize()-getDIFPtr()->getGetFramePtrReturn();
+}
+
+uint32_t SDHCAL_RawBuffer_Navigator::getDIF_CRC()
+{
+  uint32_t i{getEndOfDIFData()};
+  uint32_t ret{0};
+  ret |= ( (m_Buffer.buffer()[i-2])<<8 );
+  ret |= m_Buffer.buffer()[i-1];
+  return ret;
+}
+
+bool SDHCAL_RawBuffer_Navigator::hasSlowControlData()
+{ 
+  return getDIFBufferStart()[getEndOfDIFData()]==0xb1;
+}
+
+SDHCAL_buffer SDHCAL_RawBuffer_Navigator::getSCBuffer()
+{ 
+  setSCBuffer();
+  return m_SCbuffer;
+}
+
+bool SDHCAL_RawBuffer_Navigator::badSCData()
+{
+  setSCBuffer();
+  return m_BadSCdata;
+}
+
+void SDHCAL_RawBuffer_Navigator::setSCBuffer()
+{
+  if(! hasSlowControlData()) return;
+  if(m_SCbuffer.getsize()!=0) return; //deja fait
+  if(m_BadSCdata) return;
+  m_SCbuffer.first=&(getDIFBufferStart()[getEndOfDIFData()]);
+  //compute Slow Control size
+  uint32_t maxsize{m_Buffer.getsize()-m_DIFstartIndex-getEndOfDIFData()+1}; // should I +1 here ?
+  uint32_t k{1}; //SC Header
+  uint32_t dif_ID{m_SCbuffer.first[1]};
+  uint32_t chipSize{m_SCbuffer.first[3]};
+  while( (dif_ID != 0xa1 && m_SCbuffer.first[k] != 0xa1 && k <maxsize) ||(dif_ID == 0xa1 && m_SCbuffer.first[k+2]==chipSize && k<maxsize) )
+  {
+    k+=2; //DIF ID + ASIC Header
+    uint32_t scsize=m_SCbuffer.first[k];
+    if (scsize != 74 && scsize != 109)
+    {
+      std::cout << "PROBLEM WITH SC SIZE " << scsize << std::endl;
+      k=0;
+      m_BadSCdata=true;
+      break;
+    }
+    k++; //skip size bit
+    k+=scsize; // skip the data
+  }
+  if (m_SCbuffer.first[k] == 0xa1 && !m_BadSCdata ) m_SCbuffer.second=k+1; //add the trailer
+  else
+  {
+    m_BadSCdata=true;
+    std::cout << "PROBLEM SC TRAILER NOT FOUND " << std::endl;
+  }
+}
+
+SDHCAL_buffer SDHCAL_RawBuffer_Navigator::getEndOfAllData()
+{
+  setSCBuffer();
+  if (hasSlowControlData() && !m_BadSCdata)
+  {
+    return SDHCAL_buffer( &(m_SCbuffer.buffer()[m_SCbuffer.getsize()]), getSizeAfterDIFPtr()-3-m_SCbuffer.getsize() );
+  }
+  else return SDHCAL_buffer( &(getDIFBufferStart()[getEndOfDIFData()]), getSizeAfterDIFPtr()-3 ); //remove the 2 bytes for CRC and the DIF trailer
+}
