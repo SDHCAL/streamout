6e700134a96f5af7025efa52a070b356b0025e10
flagarde <flagarde.programs@pm.me>
2020-07-19 10:59:03 +0800

    Better encapsulation

--- core/include/DIFUnpacker.h
+++ core/include/DIFUnpacker.h
@@ -1,37 +1,10 @@
-#ifndef _DIFUnpacker_h
-#define _DIFUnpacker_h
-#include <string>
+#pragma once
+
 #include <vector>
-#include <stdint.h>
+#include <cstdint>
 
-#define DU_DATA_FORMAT_VERSION 13
-
-#define DU_START_OF_DIF      0xB0
-#define DU_START_OF_DIF_TEMP 0xBB
-#define DU_END_OF_DIF        0xA0
-#define DU_START_OF_LINES    0xC4
-#define DU_END_OF_LINES      0xD4
-
-#define DU_START_OF_FRAME    0xB4
-#define DU_END_OF_FRAME      0xA3
-
-
-#define DU_ID_SHIFT      1
-#define DU_DTC_SHIFT     2
-#define DU_GTC_SHIFT    10
-#define DU_ABCID_SHIFT  14
-#define DU_BCID_SHIFT   20
-#define DU_LINES_SHIFT  23
-#define DU_TASU1_SHIFT  24
-#define DU_TASU2_SHIFT  28
-#define DU_TDIF_SHIFT   32
-
-#define DU_FRAME_ASIC_HEADER_SHIFT 0
-#define DU_FRAME_BCID_SHIFT        1
-#define DU_FRAME_DATA_SHIFT        4
-#define DU_FRAME_SIZE              20
-
-class DIFUnpacker {
+class DIFUnpacker 
+{
 public:
   static unsigned long long GrayToBin(unsigned long long n);
   static uint32_t getStartOfDIF(unsigned char* cbuf,uint32_t size_buf,uint32_t start=92);
@@ -58,6 +31,30 @@ public:
   static uint32_t getFramePtr(std::vector<unsigned char*> &vFrame,std::vector<unsigned char*> &vLines,uint32_t max_size,unsigned char* cb,uint32_t idx=0);
   static void dumpFrameOld(unsigned char* buf);
   static unsigned long swap_bytes(unsigned int n,unsigned char* buf); //Stolen from DCBufferReader
-
+  
+private:
+  static constexpr int DU_DATA_FORMAT_VERSION{13};
+  static constexpr int DU_START_OF_DIF{0xB0};
+  static constexpr int DU_START_OF_DIF_TEMP{0xBB};
+  static constexpr int DU_END_OF_DIF{0xA0};
+  static constexpr int DU_START_OF_LINES{0xC4};
+  static constexpr int DU_END_OF_LINES{0xD4};
+  
+  static constexpr int DU_START_OF_FRAME{0xB4};
+  static constexpr int DU_END_OF_FRAME{0xA3};
+  
+  static constexpr int DU_ID_SHIFT{1};
+  static constexpr int DU_DTC_SHIFT{2};
+  static constexpr int DU_GTC_SHIFT{10};
+  static constexpr int DU_ABCID_SHIFT{14};
+  static constexpr int DU_BCID_SHIFT{20};
+  static constexpr int DU_LINES_SHIFT{23};
+  static constexpr int DU_TASU1_SHIFT{24};
+  static constexpr int DU_TASU2_SHIFT{28};
+  static constexpr int DU_TDIF_SHIFT{32};
+  
+  static constexpr int DU_FRAME_ASIC_HEADER_SHIFT{0};
+  static constexpr int DU_FRAME_BCID_SHIFT{1};
+  static constexpr int DU_FRAME_DATA_SHIFT{4};
+  static constexpr int DU_FRAME_SIZE{20};
 };
-#endif
