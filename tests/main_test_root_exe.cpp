d43d54b024f5551f0e9d77786b6cc4e240fdaaf3
flagarde <lagarde@sjtu.edu.cn>
2020-07-22 17:10:44 +0800

    Prepare for some Apps and use some magics to obtain CLI11 for parameter processing :)

--- tests/main_test_root_exe.cpp
+++ tests/main_test_root_exe.cpp
@@ -4,13 +4,40 @@
 
 #include "TFile.h"
 #include <iostream>
-int main()
+#include "CLI.hpp"
+
+int main(int argc, char** argv)
 {
-  TFile hfile("Test.root","RECREATE","Test for SDHCAL raw data file");
+  CLI::App app{"SDHCAL buffer loop with ROOT file destination"};
+  std::uint32_t eventNbr{0};
+  app.add_option("-e,--events",eventNbr, "Event number to process",0)->expected(1)->check(CLI::PositiveNumber);
+  std::uint32_t bitsToSkip{0};
+  app.add_option("-s,--skip",bitsToSkip, "Number of bits to skip from the DIF buffer",92)->expected(1)->check(CLI::PositiveNumber);
+  std::string outputFileName{"output.root"};
+  app.add_option("-o,--output",outputFileName, "Output file name","output.root")->expected(1);
+  std::string writeOption{"RECREATE"};
+  //To do ask CLI11 to check only the relevant strings
+  app.add_option("-w,--writeOption",writeOption, "Root writing option","RECREATE")->expected(1);
+  std::string title{"Test for SDHCAL raw data file"};
+  app.add_option("-t,--title",title, "Root writing option","RECREATE")->expected(1);
+  bool verbose{false};
+  app.add_flag("-v,--verbose",verbose,"Set verbosity");
+  bool debug{false};
+  app.add_flag("-d,--debug",verbose,"Set debug");
+  try 
+  {
+    app.parse(argc, argv);
+  } 
+  catch (const CLI::ParseError &e) 
+  {
+    return app.exit(e);
+  }
+  TFile hfile(outputFileName.c_str(),writeOption.c_str(),title.c_str());
   DIFdataExample source;
   ROOTtreeDest destination;
-  SDHCAL_buffer_loop<DIFdataExample,ROOTtreeDest> toto(source,destination,true,std::cout,true);
-  toto.loop();
+  SDHCAL_RawBuffer_Navigator::StartAt(bitsToSkip);
+  SDHCAL_buffer_loop<DIFdataExample,ROOTtreeDest> toto(source,destination,debug,std::cout,verbose);
+  toto.loop(eventNbr);
   std::cout<<"******************************"<<std::endl;
   toto.printAllCounters();
   std::cout<<"******************************"<<std::endl;
