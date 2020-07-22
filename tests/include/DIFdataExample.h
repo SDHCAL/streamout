f3763bdb5a25cbc7643bb26b8dadb26940c6d0b3
flagarde <lagarde@sjtu.edu.cn>
2020-07-22 17:15:02 +0800

    Merge

--- tests/include/DIFdataExample.h
+++ tests/include/DIFdataExample.h
@@ -5,15 +5,17 @@
 
 class DIFdataExample
 {
- public:
+public:
   DIFdataExample();
-  bool next();
+  bool nextEvent();
+  bool nextDIFbuffer();
   SDHCAL_buffer getSDHCALBuffer() {return SDHCAL_buffer(_RAWbuffer, BUFFER_SIZE);}
- private:
+private:
   enum {BUFFER_SIZE=143};  //94+49=143
   unsigned char _RAWbuffer[BUFFER_SIZE]; 
   bool _first;
   int _stop{0};
+  int _stopDIF{0};
 };
 
 #endif
