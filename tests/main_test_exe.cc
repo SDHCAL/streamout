ad555bdad8661fc1f1a965111a95f722909a5835
flagarde <flagarde.programs@pm.me>
2020-07-23 23:35:31 +0800

    Use CLI11

--- tests/main_test_exe.cc
+++ tests/main_test_exe.cc
@@ -2,13 +2,32 @@
 #include "textDump.h"
 #include "DIFdataExample.h"
 #include <iostream>
+#include "CLI.hpp"
 
-int main()
+int main(int argc, char** argv)
 {
+  CLI::App app{"SDHCAL buffer loop with textDump destination"};
+  std::uint32_t eventNbr{0};
+  app.add_option("-e,--events",eventNbr, "Event number to process",0)->expected(1)->check(CLI::PositiveNumber);
+  std::uint32_t bitsToSkip{0};
+  app.add_option("-s,--skip",bitsToSkip, "Number of bits to skip from the DIF buffer",92)->expected(1)->check(CLI::PositiveNumber);
+  bool verbose{false};
+  app.add_flag("-v,--verbose",verbose,"Set verbosity");
+  bool debug{false};
+  app.add_flag("-d,--debug",verbose,"Set debug");
+  try 
+  {
+    app.parse(argc, argv);
+  } 
+  catch (const CLI::ParseError &e) 
+  {
+    return app.exit(e);
+  }
+  
   DIFdataExample source;
   textDump destination;
-  SDHCAL_buffer_loop<DIFdataExample,textDump> toto(source,destination,true,std::cout,true);
-  toto.loop(2);
+  SDHCAL_buffer_loop<DIFdataExample,textDump> toto(source,destination,debug,std::cout,verbose);
+  toto.loop(eventNbr);
   std::cout<<"******************************"<<std::endl;
   toto.printAllCounters();
   std::cout<<"******************************"<<std::endl;
