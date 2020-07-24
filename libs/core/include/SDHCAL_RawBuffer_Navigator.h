9f4a66023fb05fee933a3c5ab738b7de52ac3bc1
flagarde <flagarde.programs@pm.me>
2020-07-24 23:22:46 +0800

    Add the posibility to change the starting point

--- libs/core/include/SDHCAL_RawBuffer_Navigator.h
+++ libs/core/include/SDHCAL_RawBuffer_Navigator.h
@@ -7,7 +7,7 @@
 class SDHCAL_RawBuffer_Navigator
 {
 public:
-  SDHCAL_RawBuffer_Navigator(const SDHCAL_buffer& b);
+  SDHCAL_RawBuffer_Navigator(const SDHCAL_buffer& b,const int& start=-1);
   ~SDHCAL_RawBuffer_Navigator();
   bool validBuffer();
   std::uint32_t getStartOfDIF();
@@ -22,12 +22,14 @@ public:
   SDHCAL_buffer getSCBuffer();
   bool badSCData();
   SDHCAL_buffer getEndOfAllData();
+  void static StartAt(const int& start);
 
 private:
   void setSCBuffer();
-  SDHCAL_buffer m_Buffer;
-  SDHCAL_buffer m_SCbuffer;
+  SDHCAL_buffer m_Buffer{0,0};
+  SDHCAL_buffer m_SCbuffer{0,0};
   std::uint32_t m_DIFstartIndex{0};
   DIFPtr* m_TheDIFPtr{nullptr};
   bool m_BadSCdata{false};
+  static int m_Start;
 };
