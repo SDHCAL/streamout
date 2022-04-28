4c8773f3886f47f5f9d9fb3424b13a0c7f4a13e7
flagarde <flagarde.programs@pm.me>
2022-04-28 20:21:41 +0800

    Fix compilation

--- cmake/GetCMakeMM.cmake
+++ cmake/GetCMakeMM.cmake
@@ -1,41 +1,88 @@
+# cmake-format: off
 include_guard(GLOBAL)
 
-function(cmmm)
-  include(FetchContent)
+set(GET_CMMM_VERSION "1.0.1" CACHE INTERNAL "Version of GetCMakeMM.")
 
-  cmake_parse_arguments(CMMM "NO_COLOR" "REPOSITORY;VERSION;VERBOSITY" "" ${ARGN})
-  if(NOT DEFINED CMMM_REPOSITORY)
-    set(CMMM_REPOSITORY "flagarde/CMakeMM2")
+# CMMM function
+function(cmmm)
+  cmake_parse_arguments(CMMM "NO_COLOR" "VERSION;DESTINATION;INACTIVITY_TIMEOUT;TIMEOUT;REPOSITORY;PROVIDER" "" "${ARGN}")
+
+  if(WIN32 OR (EXISTS $ENV{CLION_IDE}) OR (EXISTS $ENV{DevEnvDir}) OR (EXISTS $ENV{workspaceRoot}))
+    set(CMMM_NO_COLOR TRUE)
+  elseif(NOT DEFINED CMMM_NO_COLOR)
+    set(CMMM_NO_COLOR FALSE)
   endif()
+  set_property(GLOBAL PROPERTY CMMM_NO_COLOR ${CMMM_NO_COLOR})
 
   if(NOT DEFINED CMMM_VERSION)
-    set(CMMM_VERSION "master")
-  endif()
-
-  if(${CMMM_VERSION} STREQUAL "master")
-    FetchContent_Declare(CMakeMM GIT_REPOSITORY "https://github.com/${CMMM_REPOSITORY}" GIT_TAG "${CMMM_VERSION}" GIT_SHALLOW)
+    set(CMMM_TAG "main")
+  elseif(CMMM_VERSION STREQUAL "main")
+    set(CMMM_TAG "${CMMM_VERSION}")
   else()
-    FetchContent_Declare(CMakeMM GIT_REPOSITORY "https://github.com/${CMMM_REPOSITORY}" GIT_TAG "v${CMMM_VERSION}" GIT_SHALLOW)
+    set(CMMM_TAG "v${CMMM_VERSION}")
   endif()
 
-  if(NOT DEFINED CMAKEMM_INITIALIZED)
-    string(ASCII 27 Esc)
-    if(CMMM_NO_COLOR OR WIN32)
-      message("-- [CMakeMM] Downloading CMakeMM version \"${CMMM_VERSION}\" from \"https://github.com/${CMMM_REPOSITORY}\" --")
+  if(NOT DEFINED CMMM_DESTINATION)
+    set(CMMM_DESTINATION "${CMAKE_BINARY_DIR}/CMakeMM")
+  endif()
+  get_filename_component(CMMM_DESTINATION "${CMMM_DESTINATION}" ABSOLUTE BASE_DIR "${CMAKE_BINARY_DIR}")
+
+  if(NOT DEFINED CMMM_INACTIVITY_TIMEOUT)
+    set(CMMM_INACTIVITY_TIMEOUT "5")
+  endif()
+
+  if(NOT DEFINED CMMM_TIMEOUT)
+    set(CMMM_TIMEOUT "10")
+  endif()
+
+  string(ASCII 27 Esc)
+
+  if(NOT DEFINED CMMM_REPOSITORY)
+    set(CMMM_REPOSITORY "flagarde/CMakeMM")
+  endif()
+
+  if(NOT DEFINED CMMM_PROVIDER OR CMMM_PROVIDER STREQUAL "github")
+    set(CMMM_URL "https://raw.githubusercontent.com/${CMMM_REPOSITORY}")
+  elseif("${CMMM_PROVIDER}" STREQUAL "gitlab")
+    set(CMMM_URL "https://gitlab.com/${CMMM_REPOSITORY}/-/raw")
+  elseif("${CMMM_PROVIDER}" STREQUAL "gitee")
+    set(CMMM_URL "https://gitee.com/${CMMM_REPOSITORY}/raw")
+  else()
+    if(CMMM_NO_COLOR)
+      message("## [CMakeMM] Provider \"${CMMM_PROVIDER}\" unknown. Fallback to \"github\" ##")
     else()
-      message("${Esc}[1;35m-- [CMakeMM] Downloading CMakeMM version \"${CMMM_VERSION}\" from \"https://github.com/${CMMM_REPOSITORY}\" --${Esc}[m")
+      message("${Esc}[1;33m## [CMakeMM] Provider \"${CMMM_PROVIDER}\" unknown. Fallback to \"github\" ##${Esc}[m")
     endif()
+    set(CMMM_URL "https://raw.githubusercontent.com/${CMMM_REPOSITORY}")
   endif()
 
-  set(FETCHCONTENT_UPDATES_DISCONNECTED_CMAKEMM ON)
+  if(NOT CMAKEMM_INITIALIZED_${CMMM_TAG} OR NOT EXISTS "${CMMM_DESTINATION}/CMakeMM-${CMMM_TAG}.cmake")
 
-  FetchContent_GetProperties(CMakeMM)
+    if(CMMM_NO_COLOR)
+      message("-- [CMakeMM] Downloading CMakeMM (${CMMM_TAG}) from ${CMMM_URL}/${CMMM_TAG}/CMakeMM.cmake to ${CMMM_DESTINATION}/CMakeMM-${CMMM_TAG}.cmake --")
+    else()
+      message("${Esc}[1;35m-- [CMakeMM] Downloading CMakeMM (${CMMM_TAG}) from ${CMMM_URL}/${CMMM_TAG}/CMakeMM.cmake ${CMMM_DESTINATION}/CMakeMM-${CMMM_TAG}.cmake --${Esc}[m")
+    endif()
 
-  if(NOT cmakemm_POPULATED)
-    FetchContent_Populate(CMakeMM)
-    list(INSERT CMAKE_MODULE_PATH 0 ${cmakemm_SOURCE_DIR})
-    set(CMAKEMM_INITIALIZED "TRUE" CACHE INTERNAL "CMakeMM has been installed")
-    include(CMakeMM)
-    cmmm_entry(${ARGN})
+    file(DOWNLOAD "${CMMM_URL}/${CMMM_TAG}/CMakeMM.cmake" "${CMMM_DESTINATION}/CMakeMM-${CMMM_TAG}.cmake" INACTIVITY_TIMEOUT "${CMMM_INACTIVITY_TIMEOUT}" LOG LOG_ STATUS CMAKECM_STATUS TIMEOUT "${CMMM_TIMEOUT}")
+    list(GET CMAKECM_STATUS 0 CMAKECM_CODE)
+    list(GET CMAKECM_STATUS 1 CMAKECM_MESSAGE)
+    if(${CMAKECM_CODE})
+      if(CMMM_NO_COLOR OR (CMAKE_VERSION VERSION_GREATER_EQUAL 3.21))
+        message(FATAL_ERROR "[CMakeMM] Error downloading CMakeMM.cmake : ${CMAKECM_MESSAGE}")
+      else()
+        message(FATAL_ERROR "${Esc}[31m[CMakeMM] Error downloading CMakeMM.cmake : ${CMAKECM_MESSAGE}${Esc}[m")
+      endif()
+    endif()
+
   endif()
+
+  include("${CMMM_DESTINATION}/CMakeMM-${CMMM_TAG}.cmake")
+
+  set(ARGN "URL;${CMMM_URL};DESTINATION;${CMMM_DESTINATION};INACTIVITY_TIMEOUT;${CMMM_INACTIVITY_TIMEOUT};TIMEOUT;${CMMM_TIMEOUT};TAG;${CMMM_TAG};${ARGN}")
+  list(REMOVE_DUPLICATES ARGN)
+
+  cmmm_entry("${ARGN}")
+
 endfunction()
+# cmake-format: on
